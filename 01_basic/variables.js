3f9999e6a855b7bc94b3a91a029c86ebb7821939
Sourabh Bandgar <sourabhbandgar385@gmail.com>
2026-02-28 06:41:32 +0000

    data types

---
+++ 01_basic/variables.js
@@ -0,0 +1,19 @@
+const accountId = 13978
+let accountEmail = "sourabh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kolhapur"
+let accountState;
+
+
+accountEmail = "bandgar@gmail.com"
+accountCity = "Thane" 
+accountPassword = "45566"
+
+// accountId = 2 not allowed to change
+/*
+please do not use var 
+because od issue in 
+block scope and funcational scope
+*/
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
