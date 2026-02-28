4abf9207b1e81eed47a6d5a4a3f3b10f9f1a5c43
Sourabh Bandgar <sourabhbandgar385@gmail.com>
2026-02-28 06:40:43 +0000

    data types

---
+++ 01_basic/dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+ 
+// alert(3 + 3) // we are using nodejs not browsers console
+
+console.log(3 + 3);
+console.log(4+6)
+
+let name = "Sourabh"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number
+// bigint
+// string
+// boolean 
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "SOURABH");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
