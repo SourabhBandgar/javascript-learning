cab5b2831fca1cf4c9135f80ac32327b6cf477c6
Sourabh Bandgar <sourabhbandgar385@gmail.com>
2026-03-02 07:14:56 +0000

    data types summary

---
+++ 01_basic/comparison.js
@@ -0,0 +1,2 @@
+console.log("2" === 2); // strict check with datatypes also
+console.log("2" == 2);
---
+++ 01_basic/dataTypes_summary.js
@@ -0,0 +1,42 @@
+/*
+
+// primitive 7 types
+
+Number
+bigInt
+string
+boolean
+null 
+undefined 
+symbol
+
+// non primitive ( Reference)
+
+Array
+Objects
+functions
+
+
+*/
+
+
+// Array
+
+const newarray = ["Sourabh","Hello","Hi"];
+
+// object
+
+const newObj = {
+    name  : "Sourabh",
+    age : 22
+}
+
+// Function
+
+const newFunction = function(){
+    console.log("Hello");
+    
+}
+newFunction();
+console.log(newObj);
+console.log(newarray);
