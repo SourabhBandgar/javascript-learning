1d11df4d24cbb137226aaa449e1bbd673423f0db
Sourabh Bandgar <sourabhbandgar385@gmail.com>
2026-03-03 11:14:17 +0000

    date and time

---
+++ 01_basic/dates.js
@@ -0,0 +1,14 @@
+// Dates
+let myDate = new Date()
+//  console.log(myDate.toString());
+//  console.log(myDate.toDateString());
+//  console.log(myDate.toLocaleString());
+//  console.log(myDate.toJSON());
+ 
+//  let myTime = Date.now()
+
+//  console.log(myTime)
+
+myDate.toLocaleString('default',{
+    
+})
