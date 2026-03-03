f78783924ebad3d65b7e7018536dc09117ebc224
Sourabh Bandgar <sourabhbandgar385@gmail.com>
2026-03-03 10:24:16 +0000

    num and math

--- 01_basic/conversionOperations.js
+++ 01_basic/conversionOperations.js
@@ -1,25 +1,48 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" = 33
 // "33abc" = NaN
 // true = 1 , false = 0
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 = true
 // 0 = false
 // "SOURABH" = true
 // "" = false
 
+//................Operstions..................//
 
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+let str1 = "Hello"
+let str2 = " Sourabh"
+
+let str3 = str1+str2
+
+console.log(str3);
+console.log(1 +"2");
+console.log(1+1+"4");  // 24
+console.log("1"+1+4); // 114
---
+++ 01_basic/num_math.js
@@ -0,0 +1,18 @@
+// const value = 2003
+// console.log(value);
+
+// const balance = new Number (100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+
+// ++++++++++. MATHS ++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-10))
+// console.log(Math.floor(Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()* (max - min +1)+ min))
